57d64ce168d06720c5e5e9e6ac56f56a6a3a382d
Jura Šolja <jursolja@foi.hr>
2017-10-10 14:59:57 +0200

    seed encryption and decryption implemented (AES-256-CBC encryption)

--- BitcoinWallet/BitcoinWallet/FrmMain.Designer.cs
+++ BitcoinWallet/BitcoinWallet/FrmMain.Designer.cs
@@ -34,6 +34,12 @@
             this.inputSaveToFile = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.inputSaveToFileEnc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputEncSeed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputDecSeed = new System.Windows.Forms.TextBox();
+            this.inputDecrypt = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -67,9 +73,9 @@
             // 
             this.inputSaveToFile.Location = new System.Drawing.Point(101, 12);
             this.inputSaveToFile.Name = "inputSaveToFile";
-            this.inputSaveToFile.Size = new System.Drawing.Size(75, 23);
+            this.inputSaveToFile.Size = new System.Drawing.Size(126, 23);
             this.inputSaveToFile.TabIndex = 11;
-            this.inputSaveToFile.Text = "Save to file";
+            this.inputSaveToFile.Text = "Save to file in plain";
             this.inputSaveToFile.UseVisualStyleBackColor = true;
             this.inputSaveToFile.Click += new System.EventHandler(this.inputSaveToFile_Click);
             // 
@@ -77,11 +83,71 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // inputSaveToFileEnc
+            // 
+            this.inputSaveToFileEnc.Location = new System.Drawing.Point(233, 12);
+            this.inputSaveToFileEnc.Name = "inputSaveToFileEnc";
+            this.inputSaveToFileEnc.Size = new System.Drawing.Size(123, 23);
+            this.inputSaveToFileEnc.TabIndex = 12;
+            this.inputSaveToFileEnc.Text = "Save to file encrypted";
+            this.inputSaveToFileEnc.UseVisualStyleBackColor = true;
+            this.inputSaveToFileEnc.Click += new System.EventHandler(this.inputSaveToFileEnc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Encrypted:";
+            // 
+            // outputEncSeed
+            // 
+            this.outputEncSeed.Location = new System.Drawing.Point(101, 73);
+            this.outputEncSeed.Name = "outputEncSeed";
+            this.outputEncSeed.ReadOnly = true;
+            this.outputEncSeed.Size = new System.Drawing.Size(508, 20);
+            this.outputEncSeed.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Decrypted:";
+            // 
+            // outputDecSeed
+            // 
+            this.outputDecSeed.Location = new System.Drawing.Point(101, 185);
+            this.outputDecSeed.Name = "outputDecSeed";
+            this.outputDecSeed.ReadOnly = true;
+            this.outputDecSeed.Size = new System.Drawing.Size(508, 20);
+            this.outputDecSeed.TabIndex = 16;
+            // 
+            // inputDecrypt
+            // 
+            this.inputDecrypt.Location = new System.Drawing.Point(233, 156);
+            this.inputDecrypt.Name = "inputDecrypt";
+            this.inputDecrypt.Size = new System.Drawing.Size(123, 23);
+            this.inputDecrypt.TabIndex = 17;
+            this.inputDecrypt.Text = "Decrypt file";
+            this.inputDecrypt.UseVisualStyleBackColor = true;
+            this.inputDecrypt.Click += new System.EventHandler(this.inputDecrypt_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(627, 217);
+            this.Controls.Add(this.inputDecrypt);
+            this.Controls.Add(this.outputDecSeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputEncSeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputSaveToFileEnc);
             this.Controls.Add(this.inputSaveToFile);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.outputSeed);
@@ -101,6 +167,12 @@
         private System.Windows.Forms.Button inputSaveToFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button inputSaveToFileEnc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputEncSeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox outputDecSeed;
+        private System.Windows.Forms.Button inputDecrypt;
     }
 }
 
--- BitcoinWallet/BitcoinWallet/FrmMain.cs
+++ BitcoinWallet/BitcoinWallet/FrmMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 using NBitcoin;
 
 namespace BitcoinWallet
@@ -15,6 +16,8 @@ namespace BitcoinWallet
     public partial class FrmMain : Form
     {
         Mnemonic Seed { get; set; } = null;
+        RijndaelManaged rijndaelManaged;
+        byte[] encrypted;
         public FrmMain()
         {
             InitializeComponent();
@@ -65,9 +68,39 @@ namespace BitcoinWallet
                 sfd.ShowDialog();
                 if(sfd.FileName != "")
                 {
+
                     FileStream fs = (System.IO.FileStream)sfd.OpenFile();
                     byte[] data = new UTF8Encoding(true).GetBytes(Seed.ToString());
                     fs.Write(data, 0, data.Length);
+                    fs.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Generate a seed first.");
+            }
+        }
+
+
+        private void inputSaveToFileEnc_Click(object sender, EventArgs e)
+        {
+            if (Seed != null)
+            {
+                rijndaelManaged = new RijndaelManaged();
+                rijndaelManaged.GenerateKey();
+                rijndaelManaged.GenerateIV();
+                encrypted = EncryptStringToBytes(outputSeed.Text, rijndaelManaged.Key, rijndaelManaged.IV);
+
+                outputEncSeed.Text = Encoding.UTF8.GetString(encrypted); 
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = ".dat|*.dat";
+                sfd.Title = "wallet";
+                sfd.ShowDialog();
+                if (sfd.FileName != "")
+                {
+                    FileStream fs = (FileStream)sfd.OpenFile();
+                    fs.Write(encrypted, 0, encrypted.Length);
                     fs.Close();
                 }
                 
@@ -76,6 +109,67 @@ namespace BitcoinWallet
             {
                 MessageBox.Show("ERROR: Generate a seed first.");
             }
+        }
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encryptedText;
+            using (RijndaelManaged rAlg = new RijndaelManaged())
+            {
+                rAlg.Key = Key;
+                rAlg.IV = IV;
+                ICryptoTransform encryptor = rAlg.CreateEncryptor(rAlg.Key, rAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encryptedText = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encryptedText;
+        }
+
+        private void inputDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] cipherText;
+            Stream s = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = ".dat|*.dat";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string filename = ofd.FileName;
+                cipherText = File.ReadAllBytes(filename);
+                string decrypted = DecryptStringFromBytes(cipherText, rijndaelManaged.Key, rijndaelManaged.IV);
+                outputDecSeed.Text = decrypted;
+            }
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plainText;
+            using (RijndaelManaged rAlg = new RijndaelManaged())
+            {
+                rAlg.Key = Key;
+                rAlg.IV = IV;
+                ICryptoTransform decryptor = rAlg.CreateDecryptor(rAlg.Key, rAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plainText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plainText;
         }
     }
 }
